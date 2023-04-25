fd302f564e2c04a91b6485d3f6729ea5b50650f2
brunoby <118010824+brunoby@users.noreply.github.com>
2023-04-25 14:51:28 -0300

    The Non security API

---
+++ src/main/java/com/poc/bradesco/securityapi/NonSecurityController.java
@@ -0,0 +1,16 @@
+package com.poc.bradesco.securityapi;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/non-security")
+public class NonSecurityController {
+    
+    @GetMapping
+    public ResponseEntity<Void> nonSecurity() {
+        return ResponseEntity.ok().build();
+    }
+}
