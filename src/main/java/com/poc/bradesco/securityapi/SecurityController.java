cdd4996cd65e8f5c7341d1c6e51ed11e9784932c
brunoby <118010824+brunoby@users.noreply.github.com>
2023-04-26 15:32:03 -0300

    blooms

--- src/main/java/com/poc/bradesco/securityapi/SecurityController.java
+++ src/main/java/com/poc/bradesco/securityapi/SecurityController.java
@@ -17,8 +17,8 @@ public class SecurityController {
         
     }
 
-        /**
-     * endpoint onde o usuario tem que ter a role user
+    /**
+     * endpoint where the user has to have the role user
      */
     @GetMapping(value = "/has-role")
     @PreAuthorize("hasAnyAuthority('ROLE_USER')")
@@ -27,7 +27,7 @@ public class SecurityController {
     }
 
     /**
-     * endpoint onde o usuario tem que ter a role admin
+     * endpoint where the user has to have the role admin
      */
     @GetMapping(value = "/is-admin")
     @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
