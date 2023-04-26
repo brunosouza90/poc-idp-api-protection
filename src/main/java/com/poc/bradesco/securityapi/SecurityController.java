3cbc2808bdfb0d9227a67f61573738a09be01ccf
brunoby <118010824+brunoby@users.noreply.github.com>
2023-04-26 15:18:46 -0300

    Authorizer do usuario

--- src/main/java/com/poc/bradesco/securityapi/SecurityController.java
+++ src/main/java/com/poc/bradesco/securityapi/SecurityController.java
@@ -2,6 +2,7 @@ package com.poc.bradesco.securityapi;
 
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,24 @@ public class SecurityController {
     public ResponseEntity<Void> security() {
         return ResponseEntity.ok().build();
         
+    }
+
+        /**
+     * endpoint onde o usuario tem que ter a role user
+     */
+    @GetMapping(value = "/has-role")
+    @PreAuthorize("hasAnyAuthority('ROLE_USER')")
+    public ResponseEntity<Void> isUser() {
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     * endpoint onde o usuario tem que ter a role admin
+     */
+    @GetMapping(value = "/is-admin")
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
+    public ResponseEntity<Void> isAdmin() {
+        return ResponseEntity.ok().build();
     }
     
 }
